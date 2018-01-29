f676e17c44ea33e90e1ed4ffd7de1899973a7e0c
Mardzy <paul.mardling@itv.com>
2018-01-29 16:18:02 +0000

    create local server

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-var Axios = require('axios');
 var express = require('express');
-var socket = require('socket.io');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-// (function () {
-//     Axios.get('http://localhost:3000', {params: {arg01: 'nothing'}})
-//         .then(function(result) {
-//             console.log('then ==>', result);
-//         })
-//         .catch(function(error) {
-//             console.log('catch ==>', error);
-//         });
-// }());
+// handle http request from client side
 
-var server = app.listen(3000, function() {
-    console.log('listening to port 3000');
+
+app.post('/', function (req, res) {
+    // the "req" parameter is an object that gives you access to data in the
+    // request.
+    console.log('res ====>', res);
+    console.log('req ====>', req);
+    var body = req.body;
+
+    console.log('BODY ====>', body);
+
+    // the "res" parameter lets you manipulate the response.
+    res.end(body.param1);
+
 });
 
-var io = socket(server);
 
-io.on('connection', function(websocket) {
-    console.log('made socket connection', websocket);
+// var server = 
+app.listen(3000, function() {
+    console.log('listening to port 3000');
 });
